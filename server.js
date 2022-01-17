dd958049c74061b78f9306c222be492657dfffe5
rylee thomas <ryleetreat@gmail.com>
2022-01-17 00:22:58 -0500

    updated the HTML pages with fetch()

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 //pare incoming JSON data
 app.use(express.json());
+//instruscts the server to make certain files readily available
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -99,6 +101,23 @@ app.get('/api/animals/:id', (req, res) => {
         res.send(404);
     }
       
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+//* will act as a wildcard, any route not defined will fall under this request and recieve homepage response
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.post('/api/animals', (req,res) => {
