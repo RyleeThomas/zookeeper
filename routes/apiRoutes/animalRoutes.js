cfbff88c120cabf04f0109484a9bced799161673
rylee thomas <ryleetreat@gmail.com>
2022-01-19 16:07:40 -0500

    changed with static(public) was called

--- routes/apiRoutes/animalRoutes.js
+++ routes/apiRoutes/animalRoutes.js
@@ -1,9 +1,8 @@
-const { filterByQuery, findById, createNewAnimal, validateAnimal } = require('../../lib/animals');
-const { animals } = require('../../data/animals');
-// cannot use app because it's defined in server.js 
-const router = require('express').Router();
+const router = require("express").Router();
+const { filterByQuery, findById, createNewAnimal, validateAnimal, } = require("../../lib/animals");
+const { animals } = require("../../data/animals");
 
-router.get('/animals', (req, res) => {
+router.get("/animals", (req, res) => {
   let results = animals;
   if (req.query) {
     results = filterByQuery(req.query, results);
@@ -11,7 +10,7 @@ router.get('/animals', (req, res) => {
   res.json(results);
 });
 
-router.get('/animals/:id', (req, res) => {
+router.get("/animals/:id", (req, res) => {
   const result = findById(req.params.id, animals);
   if (result) {
     res.json(result);
@@ -20,12 +19,13 @@ router.get('/animals/:id', (req, res) => {
   }
 });
 
-router.post('/animals', (req, res) => {
+router.post("/animals", (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
 
+  // if any data in req.body is incorrect, send 400 error back
   if (!validateAnimal(req.body)) {
-    res.status(400).send('The animal is not properly formatted.');
+    res.status(400).send("The animal is not properly formatted.");
   } else {
     const animal = createNewAnimal(req.body, animals);
     res.json(animal);
--- server.js
+++ server.js
@@ -1,19 +1,23 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const { animals } = require('./data/animals');
-const PORT = process.env.PORT || 3001;
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
+const PORT = process.env.PORT || 3001;
 const app = express();
 
+
+//instruscts the server to make certain files readily available
+app.use(express.static('public'));
 //app incoming string or array data
 app.use(express.urlencoded({extended: true}));
 //pare incoming JSON data
 app.use(express.json());
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
-//instruscts the server to make certain files readily available
-app.use(express.static('public'));
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
